d85af35998460bbd8e8ae3933902b754f4c6af6d
fruh <frantisek.uhrecky@gmail.com>
2016-08-17 10:34:18 +0200

    Licence changed to beerware licence.

--- generate-images.sh
+++ generate-images.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-# This file is part of tits.
-# Copyright (C) 2016 fruh
-
-# Tits is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# Tits is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-
-# You should have received a copy of the GNU Lesser General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+# /*
+#  * ----------------------------------------------------------------------------
+#  * "THE BEER-WARE LICENSE" (Revision 42):
+#  * <fruh> wrote this file.  As long as you retain this notice you
+#  * can do whatever you want with this stuff. If we meet some day, and you think
+#  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
+#  * ----------------------------------------------------------------------------
+#  */
 
 # encode all resources to base64
 for f in $(find ./res/ -name '*.txt'); do cat $f | base64 -w 0 > ${f}.base64; done
@@ -23,21 +16,14 @@ file=src/images.py
 echo "#!/usr/bin/python" > $file
 echo "# -*- coding: utf-8 -*-" >> $file
 echo "" >> $file
-echo "# This file is part of tits." >> $file
-echo "# Copyright (C) 2016 fruh" >> $file
-echo "" >> $file
-echo "# Tits is free software: you can redistribute it and/or modify" >> $file
-echo "# it under the terms of the GNU Lesser General Public License as published by" >> $file
-echo "# the Free Software Foundation, either version 3 of the License, or" >> $file
-echo "# (at your option) any later version." >> $file
-echo "" >> $file
-echo "# Tits is distributed in the hope that it will be useful," >> $file
-echo "# but WITHOUT ANY WARRANTY; without even the implied warranty of" >> $file
-echo "# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the" >> $file
-echo "# GNU Lesser General Public License for more details." >> $file
-echo "" >> $file
-echo "# You should have received a copy of the GNU Lesser General Public License" >> $file
-echo "# along with Foobar.  If not, see <http://www.gnu.org/licenses/>." >> $file
+echo "# /*" >> $file
+echo "#  * ----------------------------------------------------------------------------" >> $file
+echo "#  * "THE BEER-WARE LICENSE" (Revision 42):" >> $file
+echo "#  * <fruh> wrote this file.  As long as you retain this notice you" >> $file
+echo "#  * can do whatever you want with this stuff. If we meet some day, and you think" >> $file
+echo "#  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp" >> $file
+echo "#  * ----------------------------------------------------------------------------" >> $file
+echo "#  */" >> $file
 echo "" >> $file
 cat sources.txt >> $file
 echo "" >> $file
--- src/tits.py
+++ src/tits.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# This file is part of tits.
-# Copyright (C) 2016 fruh
-
-# Tits is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# Tits is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-
-# You should have received a copy of the GNU Lesser General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+# /*
+#  * ----------------------------------------------------------------------------
+#  * "THE BEER-WARE LICENSE" (Revision 42):
+#  * <fruh> wrote this file.  As long as you retain this notice you
+#  * can do whatever you want with this stuff. If we meet some day, and you think
+#  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
+#  * ----------------------------------------------------------------------------
+#  */
 
 import random
 import base64
