2477fbb4c92ca74db2400922900d0c4f8a8871aa
FrUh <frantisek.uhrecky@gmail.com>
2016-08-17 15:09:13 -0500

    Added several option like print slowly, all tits, all funny. Fixed random seed.

--- src/tits.py
+++ src/tits.py
@@ -12,26 +12,60 @@
 
 import random
 import base64
+import time
 from optparse import OptionParser
 
 import images
 
+def print_image(images_base64, index, slow):
+    img = base64.decodestring(images_base64[index])
+
+    if slow:
+        img_splitted = img.split("\n")
+
+        for l in img_splitted:
+            print l
+            time.sleep(0.1)
+    else:
+        print img
+
+
+def print_all(images_base64, slow):
+    for i in range(len(images_base64)):
+        print "Picture #%d" % i
+        print_image(images_base64, i, slow)
+        print ""
+
+
 if __name__ == "__main__":
-    parser = OptionParser(usage="%prog [-f] [-h] [--select-tits N] [--select-funny N]", version="%prog 0.1", description="Show random ASCII art pictures like tits 8).")
+    parser = OptionParser(usage="%prog [options]", version="%prog 0.2", description="Show random ASCII art pictures like tits 8).")
     parser.add_option("-f", "--funny", dest="funny", action="store_true",
                       help="Show random funny picture", default=False)
+    parser.add_option("-n", "--not-slow", dest="slow", action="store_false",
+                      help="Print picture immediately", default=True)
     parser.add_option("--select-funny", dest="select_funny", action="store",
                       help="Show Nth funny picture", type="int")
     parser.add_option("--select-tits", dest="select_tits", action="store",
                       help="Show Nth tits picture", type="int")
+    parser.add_option("--all-tits", dest="all_tits", action="store_true",
+                      help="Show all tits", default=False)
+    parser.add_option("--all-funny", dest="all_funny", action="store_true",
+                      help="Show all funny", default=False)
 
     (options, args) = parser.parse_args()
 
+    # init random
+    random.seed()
+
     if options.funny:
-        print base64.decodestring(images.funlist[random.randrange(len(images.funlist))])
+        print_image(images.funlist, random.randrange(len(images.funlist)), options.slow)
     elif options.select_funny:
-        print base64.decodestring(images.funlist[options.select_funny % len(images.funlist)])
+        print_image(images.funlist, options.select_funny % len(images.funlist), options.slow)
     elif options.select_tits:
-        print base64.decodestring(images.titslist[options.select_tits % len(images.titslist)])
+        print_image(images.titslist, options.select_tits % len(images.titslist), options.slow)
+    elif options.all_tits:
+        print_all(images.titslist, options.slow)
+    elif options.all_funny:
+        print_all(images.funlist, options.slow)
     else:
-        print base64.decodestring(images.titslist[random.randrange(len(images.titslist))])
+        print_image(images.titslist, random.randrange(len(images.titslist)), options.slow)
